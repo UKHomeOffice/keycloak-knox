eebfdbf032fd959a4ae77d7d7769a2a3d911e35f
unknown <lmartins@pontusnetworks.com>
2017-10-31 22:02:29 +0000

    added build.distr.sh file; also fixed compilation issues

--- build-sandbox.sh
+++ build-sandbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 mvn -T2.0C -Drat.numUnapprovedLicenses=100 -Dmaven.test.skip=true -DskipTests=true  clean install
-scp -P 2222 gateway-service-pontus/target/gateway-service-pontus-0.9.0.jar gateway-provider-security-pontus-jwt/target/gateway-provider-security-pontus-jwt-0.9.0.jar localhost:/opt/pontus
+scp -P 2222 gateway-service-pontus/target/gateway-service-pontus-0.13.0.jar gateway-provider-security-pontus-jwt/target/gateway-provider-security-pontus-jwt-0.13.0.jar localhost:/opt/pontus
---
+++ build.distr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp */target/*.jar ../pontus-dist/pontus/pv-gdpr-api-gateway/lib
